e2845c8d09a0b26505effe74410ca27e717afea6
Adarsh Dora <adarshdora6@gmail.com>
2023-09-20 18:48:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const adress = document.getElementById("adress");
+const loc = document.getElementById("location");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
+const btn = document.querySelector(".button");
+const ipadress = document.querySelector(".ipadress");
+
+var myIcon = L.icon({
+  iconUrl: "../images/icon-location.svg",
+  iconSize: [38, 44],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowUrl: "my-icon-shadow.png",
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94],
+});
+
+const getipadress = async function (ipvalue) {
+  const response = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_aavQEDFeXGEEm6TsLTUqHQuQLJWSz&ipAddress=${ipvalue}`
+  );
+  const data = await response.json();
+  showData(data);
+  map.setView([data.location.lat, data.location.lng], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  let marker = L.marker([data.location.lat, data.location.lng], {
+    icon: myIcon,
+  }).addTo(map);
+};
+
+const showData = function (data) {
+  adress.textContent = data.ip;
+  loc.textContent = `${data.location.country}, ${data.location.region}`;
+  timezone.textContent = data.location.timezone;
+  isp.textContent = data.isp;
+};
+
+let map = L.map("map").setView([22.12, 84.54], 13);
+
+btn.addEventListener("click", async function (e) {
+  e.preventDefault();
+  getipadress(ipadress.value);
+});
+
+getipadress("");
